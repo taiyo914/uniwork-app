e1a010f3a1defe814683fccc9a7d2936c80a66b2
taiyo914 <taiyosuzuki0914@icloud.com>
2024-10-22 17:20:39 +0900

    Set LogoutBtn on top

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import LogoutButton from "@/components/LogoutBtn";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LogoutButton/>
         {children}
       </body>
     </html>
