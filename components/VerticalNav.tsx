b3301c478f6db5f1cd8660c187d1c93c9c95b99a
taiyo914 <taiyosuzuki0914@icloud.com>
2024-10-25 01:32:32 +0900

    Add LogoutButton

--- components/VerticalNav.tsx
+++ components/VerticalNav.tsx
@@ -6,13 +6,16 @@ import {
   MessageCircle,
   Users,
   User,
+  LogOut,
 } from "lucide-react";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useLogout } from "@/hooks/useLogout";
 
 export default function VerticalNav() {
   const [isExpanded, setIsExpanded] = useState(false);
   const pathname = usePathname();
+  const handleLogout = useLogout();
 
   const navItems = [
     { icon: <Home className="h-6 w-6" />, label: "Home", href: "/employee" },
@@ -28,7 +31,7 @@ export default function VerticalNav() {
 
   return (
     <nav
-      className={`p-2 fixed top-0 left-0 h-full border-r-[2px] bg-gray-50 transition-all duration-200 ease-in-out z-20 ${
+      className={`px-2 fixed top-0 left-0 h-full border-r-[2px] bg-gray-50 transition-all duration-200 ease-in-out z-20 flex flex-col justify-between ${
         isExpanded ? "w-48" : "w-16"
       }`}
       onMouseEnter={() => setIsExpanded(true)}
@@ -60,6 +63,23 @@ export default function VerticalNav() {
           </li>
         ))}
       </ul>
+      <div className="py-2 border-t border-gray-300">
+        <button
+          onClick={handleLogout}
+          className="flex w-full items-center p-3 transition-all duration-300 ease-in-out hover:bg-gray-200 rounded-lg"
+        >
+          <span className="relative mr-3">
+            <LogOut className="h-6 w-6 text-gray-600" />
+          </span>
+          <span
+            className={`whitespace-nowrap transition-all duration-200 overflow-hidden ${
+              isExpanded ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            Log out
+          </span>
+        </button>
+      </div>
     </nav>
   );
 }
