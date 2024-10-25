60e33f0f92abbcda3a0de524f6cc9be62054a174
taiyo914 <taiyosuzuki0914@icloud.com>
2024-10-25 19:04:44 +0900

    Nothing changed

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -2,6 +2,6 @@ import { Loader2 } from "lucide-react"
 
 export default function Spinner() {
   return (
-      <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+    <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
   )
 }
