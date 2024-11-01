fecbd2b49aa858ee6e64288eaa5f0f64d40787b4
taiyo914 <taiyosuzuki0914@icloud.com>
2024-11-01 10:41:46 +0900

    Implement loader and initialDataFetch

--- app/employee/[locale]/timelog/page.tsx
+++ app/employee/[locale]/timelog/page.tsx
@@ -1,8 +1,25 @@
+"use client"
 import CurrentTimeButtons from "./CurrentTimeButtons"
 import ScheduledTimeDialog from "./ScheduledTimeDialog"
 import AttendanceHistory from "./AttendanceHistory"
+import useAttendanceStore from "@/stores/useAttendanceStore";
+import { useEffect, useState } from "react";
+import { supabase } from "@/utils/supabase/client";
+import { Loader, Loader2 } from "lucide-react";
+
 
 export default function Timelog() {
+  const { loadInitialData } = useAttendanceStore();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUserData = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) await loadInitialData(user.id); //await効果あります
+      setLoading(false)
+    };
+    loadUserData();
+  }, [setLoading, loadInitialData]);
 
   return (
     <div className="min-h-screen pb-1">
@@ -11,9 +28,19 @@ export default function Timelog() {
           勤務登録画面
         </h1>
         <div className="space-y-5">
-          <CurrentTimeButtons />
-          <ScheduledTimeDialog />
-          <AttendanceHistory />
+          {loading
+            ? (
+              <div className="flex justify-center">
+                <Loader2 className="animate-spin text-blue-200" size={50}/>
+              </div>
+            ) : (
+              <>
+                <CurrentTimeButtons />
+                <ScheduledTimeDialog />
+                <AttendanceHistory />
+              </>
+            )
+          }
         </div>
       </div>
     </div>
