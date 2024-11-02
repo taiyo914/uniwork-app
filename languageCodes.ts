acfa4135e5b94ce0cf9958d6f9643f916044c4aa
taiyo914 <taiyosuzuki0914@icloud.com>
2024-11-03 00:13:12 +0900

    Just sort language codes

--- languageCodes.ts
+++ languageCodes.ts
@@ -1,24 +1,25 @@
 export const languageCodes = [
-  "en", // 英語 (English)
-  "zh", // 中国語 (Chinese)
-  "hi", // ヒンディー語 (Hindi) 
-  "es", // スペイン語 (Spanish)
-  "fr", // フランス語 (French)
   "ar", // アラビア語 (Arabic) 右から書く言語（RTL）
-  "pt", // ポルトガル語 (Portuguese)
-  "ru", // ロシア語 (Russian)
-  "ja", // 日本語 (Japanese)
-  "sw", // スワヒリ語 (Swahili) -アフリカの国々
-  "tr", // トルコ語 (Turkish)
-  "ko", // 韓国語 (Korean)
-  "it", // イタリア語 (Italian)
-  "th", // タイ語 (Thai)
+  "de", // ドイツ語 (German)
+  "en", // 英語 (English)
+  "es", // スペイン語 (Spanish)
   "fa", // ペルシャ語 (Persian) -アラブの国々 右から書く言語（RTL）
-  "uk", // ウクライナ語 (Ukrainian)
+  "fr", // フランス語 (French)
+  "hi", // ヒンディー語 (Hindi)
+  "id", // インドネシア語 (Indonesian)
+  "it", // イタリア語 (Italian)
+  "ja", // 日本語 (Japanese)
+  "ko", // 韓国語 (Korean)
+  "ms", // マレー語 (Malay) -マレーシア
   "my", // ビルマ語 (Burmese) -ミャンマー
   "ne", // ネパール語 (Nepali)
-  "id", // インドネシア語 (Indonesian)
-  "ms", // マレー語 (Malay) -マレーシア
+  "pt", // ポルトガル語 (Portuguese)
+  "ru", // ロシア語 (Russian)
+  "sw", // スワヒリ語 (Swahili) -アフリカの国々
+  "th", // タイ語 (Thai)
+  "tr", // トルコ語 (Turkish)
+  "uk", // ウクライナ語 (Ukrainian)
   "vi", // ベトナム語 (Vietnamese)
-  "de", // ドイツ語 (German)
+  "zh", // 中国語 (Chinese)
 ];
+
