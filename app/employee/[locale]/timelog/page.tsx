a6dcaa68daa298834ae2dfdf696e9ae3b392ca97
taiyo914 <taiyosuzuki0914@icloud.com>
2024-11-12 12:02:28 +0900

    Make them more compact

--- app/employee/[locale]/timelog/page.tsx
+++ app/employee/[locale]/timelog/page.tsx
@@ -5,7 +5,7 @@ import AttendanceHistory from "./AttendanceHistory"
 import useAttendanceStore from "@/stores/useAttendanceStore";
 import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase/client";
-import { Loader2 } from "lucide-react";
+import { Clock, Loader2 } from "lucide-react";
 import { useTranslation } from 'react-i18next';
 
 export default function Timelog() {
@@ -25,10 +25,10 @@ export default function Timelog() {
   }, [loadInitialData]);
 
   return (
-    <div className="px-4 sm:px-5 md:px-6 xl:px-7" >
+    <div className="px-4 sm:px-5 md:px-6 xl:px-7 bg-gray-50/50" >
       <div className="max-w-[1350px] mx-auto">
-        <h1 className="text-2xl sm:text-3xl font-bold py-5 sm:py-6 lg:py-5 text-blue-700 text-center lg:text-left">
-          {t("timelog.headerTitle")}
+        <h1 className="text-2xl sm:text-3xl font-bold py-4 sm:py-5 lg:py-5 text-blue-700  sm:max-w-[650px] sm:mx-auto  lg:max-w-full lg:text-left -mb-0.5">
+          <Clock className="sm:h-7 sm:w-7 h-6 w-6 mb-0.5 inline-block mr-1 mb-1.5"/>{t("timelog.headerTitle")}
         </h1>
         <div className=" mx-auto w-full">
           {loading
@@ -39,7 +39,7 @@ export default function Timelog() {
             ) : (
               <>
                 <div className="flex flex-col lg:flex-row lg:justify-end items-center lg:items-start  space-y-4 sm:space-y-5 lg:space-y-0 lg:space-x-6 xl:space-x-7">
-                  <div className="sm:space-y-5 space-y-4 lg:space-y-6 w-full lg:flex-1 lg:max-w-[550px] max-w-[650px]">
+                  <div className="sm:space-y-5 space-y-4 lg:space-y-5 w-full lg:flex-1 lg:max-w-[550px] max-w-[650px]">
                     <CurrentTimeButtons />
                     {workStatus === "notStarted" && <ScheduledTimeDialog />}
                   </div>
