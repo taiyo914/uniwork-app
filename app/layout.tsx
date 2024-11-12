fb0e58fac6cc5479a1c26fd08aa6fccc4d41dc85
taiyo914 <taiyosuzuki0914@icloud.com>
2024-11-12 11:59:54 +0900

    Change bg-color to white

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
         {children}
       </body>
